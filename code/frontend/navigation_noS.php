4df8c44027c003b302d7e72813b6ba245b0d3a48
Nelson Bryan <201913582B@utea.edu.pe>
2022-12-26 23:04:45 -0500

    creando nueva pagina

--- code/frontend/navigation_noS.php
+++ code/frontend/navigation_noS.php
@@ -20,7 +20,7 @@
                     <li><a href="home.php">Inicio</a></li>
                     <li><a href="access.php">Acceder</a></li>
                     <li><a href="register.php">Registrarse</a></li>
-                    <li><a href=""></a></li>
+                    <li><a href="nosostros.php"></a>Nosotros</li>
                 </ul>
             </div>
         </div>
